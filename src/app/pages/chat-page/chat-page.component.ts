dbbef2eee242c4d8b9f799ba5e92e3af0f4f7e65
Leonardo Diaz <diazleonardo237@gmail.com>
2026-03-31 16:39:59 -0500

    Cambie los estilos de la chat page y agrege logica para cambiar de chat

--- src/app/pages/chat-page/chat-page.component.ts
+++ src/app/pages/chat-page/chat-page.component.ts
@@ -1,11 +1,21 @@
 import { Component } from '@angular/core';
+import { ChatListComponent } from '../../components/chat-list/chat-list.component';
+import { Chat } from '../../../models/chat.model';
+import { ChatWindowComponent } from '../../components/chat-window/chat-window.component';
 
 @Component({
   selector: 'app-chat-page',
-  imports: [],
+  imports: [ChatListComponent,ChatWindowComponent],
   templateUrl: './chat-page.component.html',
-  styleUrl: './chat-page.component.css'
+  styleUrl: './chat-page.component.css',
 })
 export class ChatPageComponent {
-
+  chats: Chat[] = [
+    { id: 1, name: 'Ana', lastMessage: 'Hola', lastTime: '10:30' },
+    { id: 2, name: 'Carlos', lastMessage: 'Nos vemos', lastTime: '09:15' },
+  ];
+  activeConversation: Chat | undefined = undefined;
+  setConversation(chat: Chat) {
+    this.activeConversation = chat;
+  }
 }

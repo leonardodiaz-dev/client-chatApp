eb0197b030078dbd01c011183689c5e805fd7fa2
Leonardo Diaz <diazleonardo237@gmail.com>
2026-04-04 14:10:02 -0500

    Agrege codigo para llamar a la api buscar user

--- src/app/components/add-contact-modal/add-contact-modal.component.ts
+++ src/app/components/add-contact-modal/add-contact-modal.component.ts
@@ -1,14 +1,20 @@
 import { Component, inject } from '@angular/core';
-import { FormControl, ReactiveFormsModule } from '@angular/forms';
+import { FormControl, FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatAutocompleteModule } from '@angular/material/autocomplete';
 import { MatButtonModule } from '@angular/material/button';
-import { MatDialogModule } from '@angular/material/dialog';
+import {
+  MatDialogModule,
+  MatDialogRef,
+} from '@angular/material/dialog';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { User } from '../../models/user.model';
 import { UserService } from '../../services/user/user.service';
 import { MatOptionModule } from '@angular/material/core';
 import { CommonModule } from '@angular/common';
+import { ConversationService } from '../../services/conversation/conversation.service';
+import { FormConversation } from '../../models/conversation.model';
+import { NotificationService } from '../../services/notification.service';
 
 @Component({
   selector: 'app-add-contact-modal',
@@ -20,7 +26,8 @@ import { CommonModule } from '@angular/common';
     MatFormFieldModule,
     MatInputModule,
     ReactiveFormsModule,
-    CommonModule
+    FormsModule,
+    CommonModule,
   ],
   templateUrl: './add-contact-modal.component.html',
   styleUrl: './add-contact-modal.component.css',
@@ -29,17 +36,58 @@ export class AddContactModalComponent {
   control = new FormControl<string>('');
   users: User[] = [];
 
-  private userService = inject(UserService)
+  user: any = {
+    id: 0,
+    name: '',
+    lastname: '',
+  };
+
+  private userService = inject(UserService);
+  private conversationService = inject(ConversationService);
+  private dialogRef = inject(MatDialogRef<AddContactModalComponent>);
+  private notify = inject(NotificationService);
 
   ngOnInit() {
-    this.control.valueChanges.subscribe(value => {
+    this.control.valueChanges.subscribe((value) => {
       if (!value) return;
       if (value.length >= 3) {
-        this.userService.searchUser(value).subscribe(res => {
-          this.users = res
-          console.log(res)
-        })
+        this.userService.searchUser(value).subscribe((res) => {
+          this.users = res;
+          console.log(res);
+        });
       }
+    });
+  }
+
+  selectUser(user: any) {
+    this.user.id = user.id
+    this.user.name = user.name;
+    this.user.lastname = user.lastname;
+  }
+
+  addContact() {
+    const conversation: FormConversation = {
+      user_id: this.user.id,
+      type: 'private',
+    };
+   
+    this.conversationService.addConversation(conversation).subscribe({
+      next: (value) => {
+        this.notify.showSuccess(value.message);
+        this.dialogRef.close();
+      },
+      error: (err) => {
+        if (err.status === 422) {
+          const validationErrors = err.error.errors;
+
+          const firstKey = Object.keys(validationErrors)[0];
+          const errorMessage = validationErrors[firstKey][0];
+
+          this.notify.showWarning(errorMessage);
+        } else {
+          this.notify.showError('Ocurrió un error inesperado en el servidor');
+        }
+      },
     });
   }
 }

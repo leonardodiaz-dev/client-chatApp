ed01cceef4e4e6d42b17d07131f84cb9815dd93e
Leonardo Diaz <diazleonardo237@gmail.com>
2026-04-04 14:11:27 -0500

    Agrege una funcion para mostrar el nombre del contacto

--- src/app/components/chat-list/chat-list.component.ts
+++ src/app/components/chat-list/chat-list.component.ts
@@ -6,30 +6,36 @@ import {
   Output,
   viewChild,
 } from '@angular/core';
-import { Chat } from '../../../models/chat.model';
 import { MatIcon } from '@angular/material/icon';
 import { MatMenuModule, MatMenuTrigger } from '@angular/material/menu';
 import { AuthService } from '../../services/auth/auth.service';
+import { Conversation } from '../../models/conversation.model';
 
 @Component({
   selector: 'app-chat-list',
-  imports: [MatIcon,MatMenuTrigger,MatMenuModule],
+  imports: [MatIcon, MatMenuTrigger, MatMenuModule],
   templateUrl: './chat-list.component.html',
   styleUrl: './chat-list.component.css',
 })
 export class ChatListComponent {
-  @Input() chats: Chat[] = [];
+  @Input() chats: Conversation[] = [];
   @Output() changeView = new EventEmitter<'chatList' | 'newChat'>();
   @Output() selectConversation = new EventEmitter<any>();
 
   private authService = inject(AuthService);
 
-  selectChat(chat: Chat) {
+  selectChat(chat: Conversation) {
     this.selectConversation.emit(chat);
   }
 
   menuTrigger = viewChild.required(MatMenuTrigger);
 
-  logout = () => this.authService.logout()
-  
+  getChatName(chat: any) {
+    if (chat.type === 'private') {
+      return chat.users?.[0]?.name;
+    }
+
+    return chat.name;
+  }
+  logout = () => this.authService.logout();
 }

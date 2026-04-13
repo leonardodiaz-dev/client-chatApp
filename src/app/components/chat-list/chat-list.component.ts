b08e3563cfc22074e2b76b092177b8fb19a59b0d
Leonardo Diaz <diazleonardo237@gmail.com>
2026-04-13 14:57:18 -0500

    feat:agrega nuevas funciones para listar conversaciones

--- src/app/components/chat-list/chat-list.component.ts
+++ src/app/components/chat-list/chat-list.component.ts
@@ -9,25 +9,72 @@ import {
 import { MatIcon } from '@angular/material/icon';
 import { MatMenuModule, MatMenuTrigger } from '@angular/material/menu';
 import { AuthService } from '../../services/auth/auth.service';
-import { Conversation } from '../../models/conversation.model';
+import { Conversation, ConversationWithCount } from '../../models/conversation.model';
 import { ChatNamePipe } from '../../pipes/chat-name.pipe';
 import { FormatearFechaPipe } from '../../pipes/formatear-fecha.pipe';
+import { ConversationService } from '../../services/conversation/conversation.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-chat-list',
-  imports: [MatIcon, MatMenuTrigger, MatMenuModule,ChatNamePipe,FormatearFechaPipe],
+  imports: [
+    MatIcon,
+    MatMenuTrigger,
+    MatMenuModule,
+    ChatNamePipe,
+    FormatearFechaPipe,
+    CommonModule,
+  ],
   templateUrl: './chat-list.component.html',
   styleUrl: './chat-list.component.css',
 })
 export class ChatListComponent {
-  @Input() chats: Conversation[] = [];
+  @Input() chats: any = [];
   @Output() changeView = new EventEmitter<'chatList' | 'newChat'>();
   @Output() selectConversation = new EventEmitter<any>();
 
   private authService = inject(AuthService);
+  private _convesationService = inject(ConversationService);
+
+  filtro: string = 'todos';
+
+  setFiltro(value: string) {
+    this.filtro = value;
+
+    if (this.filtro === 'todos') {
+      this.getAll();
+    }
+
+    if (this.filtro === 'no leidos') {
+      this.chatsUnRead();
+    }
+  }
 
   selectChat(chat: Conversation) {
     this.selectConversation.emit(chat);
+  }
+
+  chatsUnRead() {
+    this._convesationService.getUnReadConversations().subscribe({
+      next: (value) => {
+        console.log(value)
+        this.chats = value;
+      },
+      error: (err) => {
+        console.log(err);
+      },
+    });
+  }
+
+  getAll() {
+    this._convesationService.getAllConversations().subscribe({
+      next: (value) => {
+        this.chats = value.data;
+      },
+      error: (err) => {
+        console.log(err);
+      },
+    });
   }
 
   menuTrigger = viewChild.required(MatMenuTrigger);

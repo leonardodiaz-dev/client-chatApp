8b776e524b780c77e104fc27ac4ab7cb3beedfd2
Leonardo Diaz <diazleonardo237@gmail.com>
2026-04-14 16:56:08 -0500

    feat:agrega un estado de carga

--- src/app/components/chat-list/chat-list.component.ts
+++ src/app/components/chat-list/chat-list.component.ts
@@ -3,17 +3,19 @@ import {
   EventEmitter,
   inject,
   Input,
+  OnInit,
   Output,
   viewChild,
 } from '@angular/core';
 import { MatIcon } from '@angular/material/icon';
 import { MatMenuModule, MatMenuTrigger } from '@angular/material/menu';
 import { AuthService } from '../../services/auth/auth.service';
-import { Conversation, ConversationWithCount } from '../../models/conversation.model';
+import { Conversation } from '../../models/conversation.model';
 import { ChatNamePipe } from '../../pipes/chat-name.pipe';
 import { FormatearFechaPipe } from '../../pipes/formatear-fecha.pipe';
 import { ConversationService } from '../../services/conversation/conversation.service';
 import { CommonModule } from '@angular/common';
+import { finalize, Subject, takeUntil } from 'rxjs';
 
 @Component({
   selector: 'app-chat-list',
@@ -28,21 +30,23 @@ import { CommonModule } from '@angular/common';
   templateUrl: './chat-list.component.html',
   styleUrl: './chat-list.component.css',
 })
-export class ChatListComponent {
-  @Input() chats: any = [];
+export class ChatListComponent implements OnInit {
+  @Input() chats: Conversation[] = [];
   @Output() changeView = new EventEmitter<'chatList' | 'newChat'>();
   @Output() selectConversation = new EventEmitter<any>();
 
   private authService = inject(AuthService);
-  private _convesationService = inject(ConversationService);
+  private _conversationService = inject(ConversationService);
+  private destroy$ = new Subject<void>();
 
   filtro: string = 'todos';
+  loading: boolean = false;
 
   setFiltro(value: string) {
     this.filtro = value;
 
     if (this.filtro === 'todos') {
-      this.getAll();
+      this.loadConversations();
     }
 
     if (this.filtro === 'no leidos') {
@@ -54,27 +58,59 @@ export class ChatListComponent {
     this.selectConversation.emit(chat);
   }
 
-  chatsUnRead() {
-    this._convesationService.getUnReadConversations().subscribe({
-      next: (value) => {
-        console.log(value)
-        this.chats = value;
-      },
-      error: (err) => {
-        console.log(err);
-      },
+  ngOnInit(): void {
+    this.loadConversations();
+    this._conversationService.conversationCreated$
+      .pipe(takeUntil(this.destroy$))
+      .subscribe(() => {
+        this.loadConversations();
+      });
+    this._conversationService.updateConversation$.subscribe((data) => {
+      const index = this.chats.findIndex((c) => c.id === data.conversation_id);
+      if (index !== -1) {
+        this.chats[index].last_message = data.message;
+        this.chats[index].last_date = data.created_at;
+
+        const updatedChat = this.chats.splice(index, 1)[0];
+        this.chats.unshift(updatedChat);
+      }
     });
   }
 
-  getAll() {
-    this._convesationService.getAllConversations().subscribe({
-      next: (value) => {
-        this.chats = value.data;
-      },
-      error: (err) => {
-        console.log(err);
-      },
-    });
+  ngOnDestroy() {
+    this.destroy$.next();
+    this.destroy$.complete();
+  }
+
+  chatsUnRead() {
+    this.loading = true;
+    this._conversationService
+      .getUnReadConversations()
+      .pipe(finalize(() => (this.loading = false)))
+      .subscribe({
+        next: (value) => {
+          console.log(value);
+          this.chats = value;
+        },
+        error: (err) => {
+          console.log(err);
+        },
+      });
+  }
+
+  loadConversations() {
+    this.loading = true;
+    this._conversationService
+      .getAllConversations()
+      .pipe(finalize(() => (this.loading = false)))
+      .subscribe({
+        next: (value) => {
+          this.chats = value.data;
+        },
+        error: (err) => {
+          console.log(err);
+        },
+      });
   }
 
   menuTrigger = viewChild.required(MatMenuTrigger);
